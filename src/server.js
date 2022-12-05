9bf6ead04d555f925bf9a5f4d5f1abaff25814cf
Mewerton Melo <mewerton@gmail.com>
2022-12-05 12:27:28 -0300

    Adicionando migrations

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors")
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError")
 
@@ -7,12 +7,12 @@ const express = require("express")
 
 const routes = require('./routes')
 
+migrationsRun()
+
 const app = express()
 app.use(express.json())
 
 app.use(routes)
-
-database()
 
 app.use((error, request, response, next) =>{
     if(error instanceof AppError){
