1cc1e71d4e60dde6198485580d177e64e9c5aee7
Mewerton Melo <mewerton@gmail.com>
2022-12-07 18:07:12 -0300

    Upload em funcionamento
    Falta fazer salvar na base de dados

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,12 @@
-const { Router } = require("express")
+const { Router, response } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const ProductsController = require('../controllers/ProductsController')
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const productsRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const productsController = new ProductsController()
 
@@ -13,6 +16,10 @@ productsRoutes.get("/", productsController.index)
 productsRoutes.post("/", ensureAuthenticated, productsController.create)
 productsRoutes.get("/:id", productsController.show)
 productsRoutes.delete("/:id", ensureAuthenticated, productsController.delete)
+productsRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"),(request, response) => {
+    console.log(request.file.filename)
+    response.json()
+})
 
 
 module.exports = productsRoutes
