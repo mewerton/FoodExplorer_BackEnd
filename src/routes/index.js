2f896094af4fdccedb1bf67d0ef0bc5aeb8f8252
Mewerton Melo <mewerton@gmail.com>
2022-12-06 21:23:16 -0300

    Ingredientes Controller e Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express")
 
 const usersRouter = require("./users.routes")
 const productsRouter = require("./products.routes")
+const ingredientsRoutes = require("./ingredients.routes")
 
 const routes = Router()
 
 routes.use("/users", usersRouter)
 routes.use("/products", productsRouter)
+routes.use("/ingredients", ingredientsRoutes)
 
 module.exports = routes
